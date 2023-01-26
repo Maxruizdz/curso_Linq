1751b8f9f36a0ec673ef4230dff5858305b495dd
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-01-26 10:29:52 -0300

    creacion de las clases Book y LinqQueries

---
+++ Books.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_linq
+{
+    internal class Book
+
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+         public string status { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string[] Autors { get; set; }
+
+        public string[] Categories { get; set; }
+        
+    }
+}
