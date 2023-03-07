7bd7e41ee07b412889a0ba46e4f2c032639b9aa6
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-03-07 10:08:53 -0300

    Usando operadores Where

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -8,6 +8,53 @@ namespace Curso_linq
 {
     internal class LinqQueries
     {
-        public LinqQueries() { }
+
+        private List<Book> librosCollection = new List<Book>();
+        private const string filePath = "books.json";
+        public LinqQueries() {
+
+            using (StreamReader reader = new StreamReader(filePath)) {
+
+
+                string Json = reader.ReadToEnd();
+
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(Json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+
+
+        }
+
+        public IEnumerable<Book> Libros() {
+
+
+            return librosCollection;
+
+        }
+        public IEnumerable<Book> LibrosDespuesdel2000() {
+
+            //extension method
+
+            //return librosCollection.Where(p => p.PublishedDate.Year > 2000);
+
+            //Query expresion
+
+            return from l in librosCollection
+                   where l.PublishedDate.Year > 2002
+                   select l;
+        }
+
+
+        public IEnumerable<Book> librosconmas_de250pag_TITULO_inAction() {
+
+            //Extension method
+            //return librosCollection.Where(p => p.PageCount > 250 && p.Title.Contains("in Action"));
+
+            //Query expresion
+
+            return from l in librosCollection
+                   where l.PageCount > 250 && l.Title.Contains("in Action")
+                   select l;
+        
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Curso_linq;
+
 Console.WriteLine("Hello, World!");
+
+LinqQueries queries = new LinqQueries();
+
+//ImprimirLibros(queries.Libros());
+
+//ImprimirLibros(queries.LibrosDespuesdel2000());
+
+ImprimirLibros(queries.librosconmas_de250pag_TITULO_inAction());
+
+void ImprimirLibros(IEnumerable<Book> listLibro) {
+
+
+    Console.WriteLine("{0,-60} {1,10} {2,11}\n", "Titulo", "N. Paginas ", "Fecha Publicacion");
+
+    foreach (var item in listLibro) {
+
+        Console.WriteLine("{0,-60} {1,10} {2,11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+
+
+    } 
+}
 
